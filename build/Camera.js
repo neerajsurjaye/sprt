038a120da73b3573c8ab6a60ff6427402a0d1ed7
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-04-29 19:45:34 +0530

    Implemented Camera class

---
+++ build/Camera.js
@@ -0,0 +1,67 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const HitRecord_1 = __importDefault(require("./HitRecord"));
+const Ray_1 = __importDefault(require("./Ray"));
+const Utils_1 = __importDefault(require("./Utils"));
+const Vec3_1 = __importDefault(require("./Vec3"));
+const color_1 = __importDefault(require("./color"));
+const writer_1 = __importDefault(require("./writer"));
+class Camera {
+    constructor() {
+        this.aspectRatio = 16 / 9;
+        this.imageWidth = 400;
+        this.imageHeight = Math.floor(this.imageWidth / this.aspectRatio);
+        this.imageHeight = (this.imageHeight < 1) ? 1 : this.imageHeight;
+        this.focalLength = 1.0;
+        this.viewPortHeight = 2;
+        this.viewPortWidth = this.viewPortHeight * (this.imageWidth / this.imageHeight);
+        this.cameraCenter = new Vec3_1.default(0, 0, 0);
+        console.log(`viewport w / h : ${this.viewPortWidth} , ${this.viewPortHeight}`);
+        this.viewPortU = new Vec3_1.default(this.viewPortWidth, 0, 0);
+        this.viewPortV = new Vec3_1.default(0, -this.viewPortHeight, 0);
+        this.pixelDeltaU = this.viewPortU.divide(this.imageWidth);
+        this.pixelDeltaV = this.viewPortV.divide(this.imageHeight);
+        console.log(`PixelDetlas : ${this.pixelDeltaU.vec} , ${this.pixelDeltaV.vec}`);
+        this.viewPortUpperLeft = this.cameraCenter.substract(new Vec3_1.default(0, 0, this.focalLength)).substract(this.viewPortU.divide(2)).substract(this.viewPortV.divide(2));
+        console.log(`ViewPortUpperLeft : ${this.viewPortUpperLeft.vec} `);
+        this.pixel00Loc = this.viewPortUpperLeft.add(this.pixelDeltaU.add(this.pixelDeltaV).multiply(0.5));
+        console.log(`pixel00Loc : ${this.pixel00Loc.vec}`);
+    }
+    render(world) {
+        const fileName = "out.ppm";
+        writer_1.default.clearFile(fileName);
+        writer_1.default.appendToFile(fileName, "P3\n");
+        writer_1.default.appendToFile(fileName, `${this.imageWidth} ${this.imageHeight}\n`);
+        writer_1.default.appendToFile(fileName, "255\n");
+        for (let j = 0; j < this.imageHeight; j++) {
+            console.log(`Lines remaining ${this.imageHeight - j} ----`);
+            for (let i = 0; i < this.imageWidth; i++) {
+                let pixelCenter = this.pixel00Loc.add(this.pixelDeltaU.multiply(i)).add(this.pixelDeltaV.multiply(j));
+                //explore the substraction part
+                let rayDirection = pixelCenter.substract(this.cameraCenter);
+                let ray = new Ray_1.default(this.cameraCenter, rayDirection);
+                let pixelColor = this.rayColor(ray, world);
+                writer_1.default.appendToFile(fileName, color_1.default.writeColor(pixelColor));
+            }
+        }
+    }
+    rayColor(ray, world) {
+        // let t = hitSphere(new Vec3(0 , 0 , -1) , 0.5 , ray);
+        // if(t > 0){
+        //     let N : Vec3 = ray.at(t).substract(new Vec3(0 ,0 , -1)).unitVector();
+        //     return new Vec3(N.x + 1 , N.y + 1 , N.z + 1).multiply(0.5);
+        // }
+        let rec = new HitRecord_1.default();
+        if (world.hit(ray, 0, Utils_1.default.INFINITY, rec)) {
+            return rec.normal.add(new Vec3_1.default(1, 1, 1)).multiply(0.5);
+        }
+        //colors world background
+        let unitDirection = ray.direction.unitVector();
+        let a = 0.5 * (unitDirection.y + 1); //transfrms -1 to 1 into 0 -> 1
+        return new Vec3_1.default(1, 1, 1).multiply(1.0 - a).add(new Vec3_1.default(0, 0, 1).multiply(a)); //see this part
+    }
+}
+exports.default = Camera;
--- src/app.ts
+++ src/app.ts
@@ -8,68 +8,66 @@ import HitRecord from "./HitRecord";
 import Utils from "./Utils";
 import HittableList from "./HittableList";
 import Sphere from "./Sphere";
+import Camera from "./Camera";
 
 
 
-function rayColor (ray : Ray , world : Hittable) : Vec3{
+// function rayColor (ray : Ray , world : Hittable) : Vec3{
 
-    // let t = hitSphere(new Vec3(0 , 0 , -1) , 0.5 , ray);
-    // if(t > 0){
-    //     let N : Vec3 = ray.at(t).substract(new Vec3(0 ,0 , -1)).unitVector();
-    //     return new Vec3(N.x + 1 , N.y + 1 , N.z + 1).multiply(0.5);
-    // }
+//     // let t = hitSphere(new Vec3(0 , 0 , -1) , 0.5 , ray);
+//     // if(t > 0){
+//     //     let N : Vec3 = ray.at(t).substract(new Vec3(0 ,0 , -1)).unitVector();
+//     //     return new Vec3(N.x + 1 , N.y + 1 , N.z + 1).multiply(0.5);
+//     // }
 
-    let rec : HitRecord = new HitRecord();
-    if(world.hit(ray , 0 , Utils.INFINITY , rec)){        
-        return rec.normal.add(new Vec3(1 , 1 ,1)).multiply(0.5);
-    }
+//     let rec : HitRecord = new HitRecord();
+//     if(world.hit(ray , 0 , Utils.INFINITY , rec)){        
+//         return rec.normal.add(new Vec3(1 , 1 ,1)).multiply(0.5);
+//     }
 
-    let unitDirection : Vec3 = ray.direction.unitVector();
-    let a : number  = 0.5 * (unitDirection.y + 1); //transfrms -1 to 1 into 0 -> 1
-    return new Vec3(1 , 1 , 1).multiply(1.0 - a).add(new Vec3(0 , 0 , 1).multiply(a)); //see this part
-}
+//     let unitDirection : Vec3 = ray.direction.unitVector();
+//     let a : number  = 0.5 * (unitDirection.y + 1); //transfrms -1 to 1 into 0 -> 1
+//     return new Vec3(1 , 1 , 1).multiply(1.0 - a).add(new Vec3(0 , 0 , 1).multiply(a)); //see this part
+// }
 
-function hitSphere(center : Vec3 , radius : number , ray : Ray) :number{
+// function hitSphere(center : Vec3 , radius : number , ray : Ray) :number{
 
-    // Old code 
-    // let  oc : Vec3 = center.substract(ray.origin);
-    // let a : number = ray.direction.dot(ray.direction);
-    // let b : number = -2 * ray.direction.dot(oc);
-    // let c : number = oc.dot(oc) - radius * radius;
-    // let discirminant = b * b - 4 * a * c;
+//     // Old code 
+//     // let  oc : Vec3 = center.substract(ray.origin);
+//     // let a : number = ray.direction.dot(ray.direction);
+//     // let b : number = -2 * ray.direction.dot(oc);
+//     // let c : number = oc.dot(oc) - radius * radius;
+//     // let discirminant = b * b - 4 * a * c;
 
-    // if(discirminant < 0){
-    //     return -1;
-    // }else{
-    //     return (-b - Math.sqrt(discirminant)) / (2.0 * a);
-    // }
+//     // if(discirminant < 0){
+//     //     return -1;
+//     // }else{
+//     //     return (-b - Math.sqrt(discirminant)) / (2.0 * a);
+//     // }
 
     
     
-    let oc : Vec3 = center.substract(ray.origin);
-    let a : number = ray.direction.lengthSquared();
-    let h : number = ray.direction.dot(oc);
-    let c : number = oc.lengthSquared() - radius * radius;
-    let discirminant = h * h -  a * c;
+//     let oc : Vec3 = center.substract(ray.origin);
+//     let a : number = ray.direction.lengthSquared();
+//     let h : number = ray.direction.dot(oc);
+//     let c : number = oc.lengthSquared() - radius * radius;
+//     let discirminant = h * h -  a * c;
 
-    if(discirminant < 0){
-        return -1;
-    }else{
-        return (h - Math.sqrt(discirminant)) / (a);
-    }
+//     if(discirminant < 0){
+//         return -1;
+//     }else{
+//         return (h - Math.sqrt(discirminant)) / (a);
+//     }
     
-}
+// }
 
 function main(){
 
     //width / height = 16 / 9 ----> width * 9 = height * 16
 
-    let aspectRatio : number = 16 / 9;
-    let imageWidth : number = 400;
+    
 
-    let imageHeight : number = Math.floor(imageWidth / aspectRatio);
-    imageHeight = (imageHeight < 1) ? 1 : imageHeight;
-
+    
     //world
     let world : HittableList = new HittableList();
     world.add(new Sphere(new Vec3(-0.2 , 0 , -2) , 0.5));
@@ -77,59 +75,9 @@ function main(){
     world.add(new Sphere(new Vec3(-1 , 0 , -2) , 0.5));
     world.add(new Sphere(new Vec3(0 , -100.5, -1) , 100));
 
-    let focalLength = 1.0;
-    let viewPortHeight : number = 2;
-    let viewPortWidth : number = viewPortHeight * (imageWidth / imageHeight); 
-    let cameraCenter : Vec3  = new Vec3(0 , 0 , 0);
-
-    log(`viewport w / h : ${viewPortWidth} , ${viewPortHeight}`);
-
-    let viewPortU : Vec3 = new Vec3(viewPortWidth , 0 , 0);
-    let viewPortV : Vec3 = new Vec3(0 , -viewPortHeight , 0);
-
-    let pixelDeltaU  : Vec3 = viewPortU.divide(imageWidth);
-    let pixelDeltaV  : Vec3  = viewPortV.divide(imageHeight);
-
-    console.log(`PixelDetlas : ${pixelDeltaU.vec} , ${pixelDeltaV.vec}`);
-
-    let viewPortUpperLeft = cameraCenter.substract(new Vec3(0 , 0 , focalLength)).substract(viewPortU.divide(2)).substract(viewPortV.divide(2));
-
-    console.log(`ViewPortUpperLeft : ${viewPortUpperLeft.vec} `);
-
-    let pixel00Loc = viewPortUpperLeft.add(pixelDeltaU.add(pixelDeltaV).multiply(0.5));
+    let camera : Camera = new Camera();
+    camera.render(world);
     
-    console.log(`pixel00Loc : ${pixel00Loc.vec}`);
-
-
-
-
-
-
-    const fileName = "out.ppm";
-    writer.clearFile(fileName)
-  
-
-
-    writer.appendToFile(fileName , "P3\n");
-    writer.appendToFile(fileName , `${imageWidth} ${imageHeight}\n`);
-    writer.appendToFile(fileName , "255\n");
-
-    for(let j = 0 ; j < imageHeight ; j++){
-        console.log(`Lines remaining ${imageHeight - j} ----`);
-        for(let i = 0 ; i < imageWidth ; i++){
-            let pixelCenter = pixel00Loc.add(pixelDeltaU.multiply(i)).add(pixelDeltaV.multiply(j));
-
-            //explore the substraction part
-            let rayDirection = pixelCenter.substract(cameraCenter);
-            let ray : Ray = new Ray(cameraCenter , rayDirection);
-
-            let pixelColor : Vec3 = rayColor(ray , world);
-
-            writer.appendToFile(fileName , Color.writeColor(pixelColor));
-
-        }
-    }
-
     console.log("Done Rendering :)");
 
 }
