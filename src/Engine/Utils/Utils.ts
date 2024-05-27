8ec03efc42f3358920d3f80fa87cd0f74573d9b5
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-05-27 21:27:05 +0530

    Fixed sonar defects

--- src/Engine/Utils/Utils.ts
+++ src/Engine/Utils/Utils.ts
@@ -9,101 +9,117 @@ import Color from "../Vectors/Color";
 import Vec3 from "../Vectors/Vec3";
 import Camera from "./Camera";
 
-class Utils{
-    static _infinity : number = Number.POSITIVE_INFINITY;
-    static _pi : number = 3.1415926535897932385;
+class Utils {
+    static readonly _infinity: number = Number.POSITIVE_INFINITY;
+    static readonly _pi: number = 3.1415926535897932385;
 
-    static get INFINITY(){
-        return this._infinity; 
+    static get INFINITY() {
+        return this._infinity;
     }
 
-    static get PI(){
+    static get PI() {
         return this._pi;
     }
 
-    static degreesToRadians(degrees : number) : number{
-        return degrees * this.PI / 180;
+    static degreesToRadians(degrees: number): number {
+        return (degrees * this.PI) / 180;
     }
 
-    static randomDouble() : number{
+    static randomDouble(): number {
         return Math.random();
     }
 
-    static randomDoubleRange(min : number , max : number){
+    static randomDoubleRange(min: number, max: number) {
         return min + (max - min) * this.randomDouble();
     }
 
-    static configWorld(config : Array<any> , world : HittableList){
+    static configWorld(config: Array<any>, world: HittableList) {
         world.clear();
         console.log(config);
-        
-        if(!config) return;
 
+        if (!config) return;
 
-        for(let i : number = 0 ; i < config.length ; i++){
-            let objectConfig : any = config[i];
+        for (const element of config) {
+            let objectConfig: any = element;
 
             world.add(this.createObject(objectConfig));
-
         }
-
     }
 
-    static createObject(objectConfig : any) : Hittable{
-
-        if(objectConfig.objectType == 'sphere'){
+    static createObject(objectConfig: any): Hittable {
+        if (objectConfig.objectType == "sphere") {
             // let objLoc = objectConfig.location;
-            return new Sphere(new Vec3(objectConfig.locx , objectConfig.locy , objectConfig.locz) , objectConfig.radius , this.createMaterial(objectConfig.material));
+            return new Sphere(
+                new Vec3(
+                    objectConfig.locx,
+                    objectConfig.locy,
+                    objectConfig.locz
+                ),
+                objectConfig.radius,
+                this.createMaterial(objectConfig.material)
+            );
         }
 
-        
-        return new Sphere(new Vec3(0 , 0 , 0) , 1 , new Lambertian(new Color(1 , 0 , 0)));
-
+        return new Sphere(
+            new Vec3(0, 0, 0),
+            1,
+            new Lambertian(new Color(1, 0, 0))
+        );
     }
 
-    static createMaterial(materialConfig : any) : Material{
-
-        if(!materialConfig){
-            return new Lambertian(new Color(1 , 0 , 0));
+    static createMaterial(materialConfig: any): Material {
+        if (!materialConfig) {
+            return new Lambertian(new Color(1, 0, 0));
         }
 
-        if(materialConfig.materialType == 'lambertian'){
+        if (materialConfig.materialType == "lambertian") {
             let matColor = materialConfig.color;
-            return new Lambertian(new Color(matColor.r , matColor.g, matColor.b));
+            return new Lambertian(
+                new Color(matColor.r, matColor.g, matColor.b)
+            );
         }
 
-        if(materialConfig.materialType == 'dielectric'){
+        if (materialConfig.materialType == "dielectric") {
             return new Dielectric(materialConfig.ir);
         }
 
-        if(materialConfig.materialType == 'metal'){
+        if (materialConfig.materialType == "metal") {
             let matColor = materialConfig.color;
-            return new Metal(new Color(matColor.r , matColor.g, matColor.b), materialConfig.fuzz);
-        } 
+            return new Metal(
+                new Color(matColor.r, matColor.g, matColor.b),
+                materialConfig.fuzz
+            );
+        }
 
-        return new Lambertian(new Color(1 , 0 , 0));
-
+        return new Lambertian(new Color(1, 0, 0));
     }
 
-    static configCamera(cameraConfig : any , camera : Camera) : void {
-        camera.aspectRatio = cameraConfig.aspectRatio ? cameraConfig.aspectRatio : 16/9;
-        camera.imageWidth = cameraConfig.imageWidth ? cameraConfig.imageWidth : 400;
-        let lookFrom = cameraConfig.lookFrom ? cameraConfig.lookFrom : {x : 0 , y : 0 , z : 0};
-        let lookAt = cameraConfig.lookAt ? cameraConfig.lookAt : {x: 1 , y : 0 , z : 0};
+    static configCamera(cameraConfig: any, camera: Camera): void {
+        camera.aspectRatio = cameraConfig.aspectRatio
+            ? cameraConfig.aspectRatio
+            : 16 / 9;
+        camera.imageWidth = cameraConfig.imageWidth
+            ? cameraConfig.imageWidth
+            : 400;
+        let lookFrom = cameraConfig.lookFrom
+            ? cameraConfig.lookFrom
+            : { x: 0, y: 0, z: 0 };
+        let lookAt = cameraConfig.lookAt
+            ? cameraConfig.lookAt
+            : { x: 1, y: 0, z: 0 };
 
-        camera.lookFrom = new Vec3(lookFrom.x , lookFrom.y , lookFrom.z);
-        camera.lookAt = new Vec3(lookAt.x , lookAt.y , lookAt.z);
+        camera.lookFrom = new Vec3(lookFrom.x, lookFrom.y, lookFrom.z);
+        camera.lookAt = new Vec3(lookAt.x, lookAt.y, lookAt.z);
 
-        
         camera.vfov = cameraConfig.vfov ? cameraConfig.vfov : 45;
-        camera.defocusAngle = cameraConfig.defocusAngle >= 0 ? cameraConfig.defocusAngle : 2;
-        camera.samplePerPixel = cameraConfig.samplePerPixel ? cameraConfig.samplePerPixel : 20;
+        camera.defocusAngle =
+            cameraConfig.defocusAngle >= 0 ? cameraConfig.defocusAngle : 2;
+        camera.samplePerPixel = cameraConfig.samplePerPixel
+            ? cameraConfig.samplePerPixel
+            : 20;
         camera.maxDepth = cameraConfig.maxDepth ? cameraConfig.maxDepth : 4;
         camera.initialize();
     }
-
-    
-    
 }
 
 export default Utils;
