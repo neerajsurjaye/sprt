3fa6c24b0d9a20f93d8e88bdb9d0e2aa5d023f1a
Neeraj Surjaye <neerajsurjaye@gmail.com>
2024-06-11 23:06:33 +0530

    Fixed normal rendering

--- src/Electron/index.ts
+++ src/Electron/index.ts
@@ -40,7 +40,7 @@ app.whenReady().then(() => {
 
     let sprt: Core = new Core(renderImage);
     ipcMain.handle("render", (event: any, config: any) => {
-        return sprt.renderNormal("channelx", config);
+        return sprt.render("channelx", config);
     });
     ipcMain.handle("refresh", (config: any) => {
         sprt.updateConfig(config);
